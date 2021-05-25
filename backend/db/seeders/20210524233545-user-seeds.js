7be467108eb5129d3b520d5338daa713b736ab44
Nathan Blaz <nathanblaz@gmail.com>
2021-05-24 22:11:42 -0700

    Seed files all populated. Moving through testing

--- backend/db/seeders/20210524233545-user-seeds.js
+++ backend/db/seeders/20210524233545-user-seeds.js
@@ -1,137 +1,145 @@
-'use strict';
-const faker = require('faker');
-const bcrypt = require('bcryptjs');
+"use strict";
+const faker = require("faker");
+const bcrypt = require("bcryptjs");
 
 module.exports = {
   up: (queryInterface, Sequelize) => {
-    return queryInterface.bulkInsert('Users', [
-      {
-        email: 'demo@user.io',
-        username: 'Demo-lition',
-        hashedPassword: bcrypt.hashSync('password'),
-        avatar: faker.internet.avatar(),
-      },
-      {
-        email: faker.internet.email(),
-        username: 'MarkB',
-        hashedPassword: bcrypt.hashSync('password'),
-        avatar: faker.internet.avatar(),
-      },
-      {
-        email: faker.internet.email(),
-        username: 'JasonJ',
-        hashedPassword: bcrypt.hashSync('password'),
-        avatar: faker.internet.avatar(),
-      },
-      {
-        email: faker.internet.email(),
-        username: 'KristinaG',
-        hashedPassword: bcrypt.hashSync('password'),
-        avatar: faker.internet.avatar(),
-      },
-      {
-        email: faker.internet.email(),
-        username: 'AlyssaK',
-        hashedPassword: bcrypt.hashSync('password'),
-        avatar: faker.internet.avatar(),
-      },
-      {
-        email: faker.internet.email(),
-        username: 'DerekG',
-        hashedPassword: bcrypt.hashSync('password'),
-        avatar: faker.internet.avatar(),
-      },
-      {
-        email: faker.internet.email(),
-        username: 'TedH',
-        hashedPassword: bcrypt.hashSync('password'),
-        avatar: faker.internet.avatar(),
-      },
-      {
-        email: faker.internet.email(),
-        username: 'ConradE',
-        hashedPassword: bcrypt.hashSync('password'),
-        avatar: faker.internet.avatar(),
-      },
-      {
-        email: faker.internet.email(),
-        username: 'ChrisV',
-        hashedPassword: bcrypt.hashSync('password'),
-        avatar: faker.internet.avatar(),
-      },
-      {
-        email: faker.internet.email(),
-        username: 'JasonJ',
-        hashedPassword: bcrypt.hashSync('password'),
-        avatar: faker.internet.avatar(),
-      },
-      {
-        email: faker.internet.email(),
-        username: 'JerryM',
-        hashedPassword: bcrypt.hashSync('password'),
-        avatar: faker.internet.avatar(),
-      },
-      {
-        email: faker.internet.email(),
-        username: 'JasonJ',
-        hashedPassword: bcrypt.hashSync('password'),
-        avatar: faker.internet.avatar(),
-      },
-      {
-        email: faker.internet.email(),
-        username: 'JasonJ',
-        hashedPassword: bcrypt.hashSync('password'),
-        avatar: faker.internet.avatar(),
-      },
-      {
-        email: faker.internet.email(),
-        username: 'JasonJ',
-        hashedPassword: bcrypt.hashSync('password'),
-        avatar: faker.internet.avatar(),
-      },
-      {
-        email: faker.internet.email(),
-        username: 'HampusP',
-        hashedPassword: bcrypt.hashSync('password'),
-        avatar: faker.internet.avatar(),
-      },
-      {
-        email: faker.internet.email(),
-        username: 'PatrickF',
-        hashedPassword: bcrypt.hashSync('password'),
-        avatar: faker.internet.avatar(),
-      },
-      {
-        email: faker.internet.email(),
-        username: 'CodyD',
-        hashedPassword: bcrypt.hashSync('password'),
-        avatar: faker.internet.avatar(),
-      },
-      {
-        email: faker.internet.email(),
-        username: 'JohannG',
-        hashedPassword: bcrypt.hashSync('password'),
-        avatar: faker.internet.avatar(),
-      },
-      {
-        email: faker.internet.email(),
-        username: 'ArloS',
-        hashedPassword: bcrypt.hashSync('password'),
-        avatar: faker.internet.avatar(),
-      },
-      {
-        email: faker.internet.email(),
-        username: 'LynnL',
-        hashedPassword: bcrypt.hashSync('password'),
-        avatar: faker.internet.avatar(),
-      },
-    ], {});
+    return queryInterface.bulkInsert(
+      "Users",
+      [
+        {
+          email: "demo@user.io",
+          username: "Demo-lition",
+          hashedPassword: bcrypt.hashSync("password"),
+          avatar: faker.internet.avatar(),
+        },
+        {
+          email: faker.internet.email(),
+          username: "MarkB",
+          hashedPassword: bcrypt.hashSync("password"),
+          avatar: faker.internet.avatar(),
+        },
+        {
+          email: faker.internet.email(),
+          username: "JasonJ",
+          hashedPassword: bcrypt.hashSync("password"),
+          avatar: faker.internet.avatar(),
+        },
+        {
+          email: faker.internet.email(),
+          username: "KristinaG",
+          hashedPassword: bcrypt.hashSync("password"),
+          avatar: faker.internet.avatar(),
+        },
+        {
+          email: faker.internet.email(),
+          username: "AlyssaK",
+          hashedPassword: bcrypt.hashSync("password"),
+          avatar: faker.internet.avatar(),
+        },
+        {
+          email: faker.internet.email(),
+          username: "DerekG",
+          hashedPassword: bcrypt.hashSync("password"),
+          avatar: faker.internet.avatar(),
+        },
+        {
+          email: faker.internet.email(),
+          username: "TedH",
+          hashedPassword: bcrypt.hashSync("password"),
+          avatar: faker.internet.avatar(),
+        },
+        {
+          email: faker.internet.email(),
+          username: "ConradE",
+          hashedPassword: bcrypt.hashSync("password"),
+          avatar: faker.internet.avatar(),
+        },
+        {
+          email: faker.internet.email(),
+          username: "ChrisV",
+          hashedPassword: bcrypt.hashSync("password"),
+          avatar: faker.internet.avatar(),
+        },
+        {
+          email: faker.internet.email(),
+          username: "JasonJ",
+          hashedPassword: bcrypt.hashSync("password"),
+          avatar: faker.internet.avatar(),
+        },
+        {
+          email: faker.internet.email(),
+          username: "JerryM",
+          hashedPassword: bcrypt.hashSync("password"),
+          avatar: faker.internet.avatar(),
+        },
+        {
+          email: faker.internet.email(),
+          username: "JasonJ",
+          hashedPassword: bcrypt.hashSync("password"),
+          avatar: faker.internet.avatar(),
+        },
+        {
+          email: faker.internet.email(),
+          username: "JasonJ",
+          hashedPassword: bcrypt.hashSync("password"),
+          avatar: faker.internet.avatar(),
+        },
+        {
+          email: faker.internet.email(),
+          username: "JasonJ",
+          hashedPassword: bcrypt.hashSync("password"),
+          avatar: faker.internet.avatar(),
+        },
+        {
+          email: faker.internet.email(),
+          username: "HampusP",
+          hashedPassword: bcrypt.hashSync("password"),
+          avatar: faker.internet.avatar(),
+        },
+        {
+          email: faker.internet.email(),
+          username: "PatrickF",
+          hashedPassword: bcrypt.hashSync("password"),
+          avatar: faker.internet.avatar(),
+        },
+        {
+          email: faker.internet.email(),
+          username: "CodyD",
+          hashedPassword: bcrypt.hashSync("password"),
+          avatar: faker.internet.avatar(),
+        },
+        {
+          email: faker.internet.email(),
+          username: "JohannG",
+          hashedPassword: bcrypt.hashSync("password"),
+          avatar: faker.internet.avatar(),
+        },
+        {
+          email: faker.internet.email(),
+          username: "ArloS",
+          hashedPassword: bcrypt.hashSync("password"),
+          avatar: faker.internet.avatar(),
+        },
+        {
+          email: faker.internet.email(),
+          username: "LynnL",
+          hashedPassword: bcrypt.hashSync("password"),
+          avatar: faker.internet.avatar(),
+        },
+      ],
+      {}
+    );
   },
 
   down: (queryInterface, Sequelize) => {
     const Op = Sequelize.Op;
-    return queryInterface.bulkDelete('Users', {
-      username: { [Op.in]: ['Demo-lition', 'FakeUser1', 'FakeUser2'] }
-    }, {});
-  }
+    return queryInterface.bulkDelete(
+      "Users",
+      {
+        username: { [Op.in]: ["Demo-lition", "FakeUser1", "FakeUser2"] },
+      },
+      {}
+    );
+  },
 };
--- backend/db/seeders/20210524233559-bagel-seeds.js
+++ backend/db/seeders/20210524233559-bagel-seeds.js
@@ -1,60 +1,64 @@
-'use strict';
+"use strict";
 
 module.exports = {
   up: (queryInterface, Sequelize) => {
-    return queryInterface.bulkInsert('Bagels', [
-    {
-      bagelType: 'Plain',
-    },
-    {
-      bagelType: 'Everything',
-    },
-    {
-      bagelType: 'Cinnamon Raisin',
-    },
-    {
-      bagelType: 'Blueberry',
-    },
-    {
-      bagelType: 'Chocolate Chip',
-    },
-    {
-      bagelType: 'Onion',
-    },
-    {
-      bagelType: 'Asiago',
-    },
-    {
-      bagelType: 'Salt',
-    },
-    {
-      bagelType: 'Poppy Seed',
-    },
-    {
-      bagelType: 'French Toast',
-    },
-    {
-      bagelType: 'Egg',
-    },
-    {
-      bagelType: 'Pumpernickel',
-    },
-    {
-      bagelType: 'Whole Wheat',
-    },
-    {
-      bagelType: 'Garlic',
-    },
-    {
-      bagelType: 'Sesame',
-    },
-    {
-      bagelType: 'Cheddar',
-    }
-  ], {});
+    return queryInterface.bulkInsert(
+      "Bagels",
+      [
+        {
+          bagelType: "Plain",
+        },
+        {
+          bagelType: "Everything",
+        },
+        {
+          bagelType: "Cinnamon Raisin",
+        },
+        {
+          bagelType: "Blueberry",
+        },
+        {
+          bagelType: "Chocolate Chip",
+        },
+        {
+          bagelType: "Onion",
+        },
+        {
+          bagelType: "Asiago",
+        },
+        {
+          bagelType: "Salt",
+        },
+        {
+          bagelType: "Poppy Seed",
+        },
+        {
+          bagelType: "French Toast",
+        },
+        {
+          bagelType: "Egg",
+        },
+        {
+          bagelType: "Pumpernickel",
+        },
+        {
+          bagelType: "Whole Wheat",
+        },
+        {
+          bagelType: "Garlic",
+        },
+        {
+          bagelType: "Sesame",
+        },
+        {
+          bagelType: "Cheddar",
+        },
+      ],
+      {}
+    );
   },
 
   down: (queryInterface, Sequelize) => {
-    return queryInterface.bulkDelete('Bagels', null, {});
-  }
+    return queryInterface.bulkDelete("Bagels", null, {});
+  },
 };

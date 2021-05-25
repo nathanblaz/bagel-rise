3d0682ed25125bd4af93fd32ad2770676e365ecb
Nathan Blaz <nathanblaz@gmail.com>
2021-05-24 19:40:51 -0700

    Bagel seed file generated

---
+++ backend/db/seeders/20210524233559-bagel-seeds.js
@@ -0,0 +1,60 @@
+'use strict';
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.bulkInsert('Bagels', [
+    {
+      bagelType: 'Plain',
+    },
+    {
+      bagelType: 'Everything',
+    },
+    {
+      bagelType: 'Cinnamon Raisin',
+    },
+    {
+      bagelType: 'Blueberry',
+    },
+    {
+      bagelType: 'Chocolate Chip',
+    },
+    {
+      bagelType: 'Onion',
+    },
+    {
+      bagelType: 'Asiago',
+    },
+    {
+      bagelType: 'Salt',
+    },
+    {
+      bagelType: 'Poppy Seed',
+    },
+    {
+      bagelType: 'French Toast',
+    },
+    {
+      bagelType: 'Egg',
+    },
+    {
+      bagelType: 'Pumpernickel',
+    },
+    {
+      bagelType: 'Whole Wheat',
+    },
+    {
+      bagelType: 'Garlic',
+    },
+    {
+      bagelType: 'Sesame',
+    },
+    {
+      bagelType: 'Cheddar',
+    }
+  ], {});
+  },
+
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.bulkDelete('Bagels', null, {});
+  }
+};

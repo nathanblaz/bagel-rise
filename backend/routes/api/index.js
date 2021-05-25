c92daf3c62a8467a207b68978784637646618435
Nathan Blaz <nathanblaz@gmail.com>
2021-05-25 14:09:56 -0700

    Checkins backend route functional

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,9 +1,12 @@
 const router = require('express').Router();
 const sessionRouter = require('./session.js');
 const usersRouter = require('./users.js');
+const checkinsRouter = require('./checkins.js');
 
 router.use('/session', sessionRouter);
 
 router.use('/users', usersRouter);
+
+router.use('/checkins', checkinsRouter);
 
 module.exports = router;

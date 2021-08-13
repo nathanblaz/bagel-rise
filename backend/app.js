b46f5e3885abced1a0b3bc6c724763b99758c578
Nathan Blaz <nathanblaz@gmail.com>
2021-08-12 19:16:20 -0700

    Express Middleware SSL Redirect for Node on Heroku

--- backend/app.js
+++ backend/app.js
@@ -1,16 +1,16 @@
-const express = require('express');
-const morgan = require('morgan');
-const cors = require('cors');
-const csurf = require('csurf');
-const helmet = require('helmet');
-const cookieParser = require('cookie-parser');
-const { environment } = require('./config');
-const isProduction = environment === 'production';
+const express = require("express");
+const morgan = require("morgan");
+const cors = require("cors");
+const csurf = require("csurf");
+const helmet = require("helmet");
+const cookieParser = require("cookie-parser");
+const { environment } = require("./config");
+const isProduction = environment === "production";
 const app = express();
-const routes = require('./routes');
-const { ValidationError } = require('sequelize');
+const routes = require("./routes");
+const { ValidationError } = require("sequelize");
 
-app.use(morgan('dev'));
+app.use(morgan("dev"));
 
 app.use(cookieParser());
 app.use(express.json());
@@ -21,9 +21,11 @@ if (!isProduction) {
   app.use(cors());
 }
 // helmet helps set a variety of headers to better secure your app
-app.use(helmet({
-  contentSecurityPolicy: false
-}));
+app.use(
+  helmet({
+    contentSecurityPolicy: false,
+  })
+);
 
 // Set the _csrf token and create req.csrfToken method
 app.use(
@@ -34,11 +36,22 @@ app.use(
       httpOnly: true,
     },
   })
-  );
-  
-  app.use(routes); // Connect all the routes
-  
-  // Catch unhandled requests and forward to error handler.
+);
+
+// Express Middleware SSL Redirect for Node on Heroku
+if (isProduction) {
+  app.use((req, res, next) => {
+    if (req.header("x-forwarded-proto") !== "https") {
+      res.redirect(`https://${req.header("host")}${req.url}`);
+    } else {
+      next();
+    }
+  });
+}
+
+app.use(routes); // Connect all the routes
+
+// Catch unhandled requests and forward to error handler.
 app.use((_req, _res, next) => {
   const err = new Error("The requested resource couldn't be found.");
   err.title = "Resource Not Found";
@@ -52,7 +65,7 @@ app.use((err, _req, _res, next) => {
   // check if error is a Sequelize error:
   if (err instanceof ValidationError) {
     err.errors = err.errors.map((e) => e.message);
-    err.title = 'Validation error';
+    err.title = "Validation error";
   }
   next(err);
 });
@@ -62,11 +75,11 @@ app.use((err, _req, res, _next) => {
   res.status(err.status || 500);
   console.error(err);
   res.json({
-    title: err.title || 'Server Error',
+    title: err.title || "Server Error",
     message: err.message,
     errors: err.errors,
     stack: isProduction ? null : err.stack,
   });
 });
 
-  module.exports = app;
+module.exports = app;
